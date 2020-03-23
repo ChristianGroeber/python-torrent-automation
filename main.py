3cbed651940b8f414219e7c9159584c945abc78d
ChristianGroeber <christian.groeber@bluewin.ch>
2020-03-23 10:00:03 +0100

    added gui with tkinter

--- main.py
+++ main.py
@@ -1,23 +1,69 @@
-import feedparser
-import Torrent
-import urllib.request
 import os
+import sched
+import time
+import urllib.request
+from tkinter import *
+import threading
+import datetime
+
+import feedparser
+
+import Torrent
 import settings
-import sched, time
+import sys
 
+master = Tk()
 
-schedule = sched.scheduler(time.time, time.sleep)
+pause_btn_text = StringVar()
+pause_btn_text.set('Pause')
+
 torrents = []
 all_torrents = []
 
+schedule = sched.scheduler(time.time, time.sleep)
+
+updates = 1
+log_file = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + ".log"
+
+
+def get_time_str():
+    return datetime.datetime.now().strftime('%H:%M:%S')
+
+
+def update_settings():
+    new_watch_dir = watch_dir.get()
+    if new_watch_dir != settings.WATCH_DIR:
+        write_log('updated watch dir to' + new_watch_dir)
+        settings.WATCH_DIR = new_watch_dir
+
+    new_rss_feed = rss_feed.get()
+    if new_rss_feed != settings.RSS_FEED:
+        write_log('updated rss feed to ' + new_rss_feed)
+        settings.RSS_FEED = new_rss_feed
+
+    new_update_after_minutes = float(update_after_minutes.get())
+    if new_update_after_minutes != settings.UPDATE_AFTER_MINUTES:
+        write_log('updated update after minutes to ' + str(new_update_after_minutes))
+        settings.UPDATE_AFTER_MINUTES = new_update_after_minutes
+
+
+def toggle_active():
+    if settings.AUTOMATIC_UPDATE:
+        settings.AUTOMATIC_UPDATE = False
+        pause_btn_text.set('Continue')
+    else:
+        settings.AUTOMATIC_UPDATE = True
+        pause_btn_text.set('Pause')
+        create_thread()
+
 
 def download(torrent):
-    print('adding torrent ' + torrent.title)
+    write_log('downloading' + torrent.title)
     urllib.request.urlretrieve(torrent.link, os.path.join(settings.WATCH_DIR, str(torrent.id) + ".torrent"))
 
 
 def update(sc, update_counter):
-    print('Running update ' + str(update_counter))
+    write_log('running update ' + str(update_counter))
     news_feed = feedparser.parse(settings.RSS_FEED)
     for torr in news_feed.entries:
         parsed = Torrent.Torrent(torr)
@@ -29,11 +75,65 @@ def update(sc, update_counter):
             else:
                 torrents.append(parsed)
                 download(parsed)
-    print('finished update ' + str(update_counter))
+    write_log("finished running update " + str(update_counter))
+    update_counter += 1
+    global updates
+    updates = update_counter
+
     if settings.AUTOMATIC_UPDATE:
-        update_counter += 1
         schedule.enter(60 * settings.UPDATE_AFTER_MINUTES, 1, update, (sc, update_counter))
 
 
-schedule.enter(1, 1, update, (schedule, 1))
-schedule.run()
+def create_thread():
+    thread = threading.Thread(target=schedule.run, args=(1,))
+    schedule.enter(1, 1, update, (schedule, updates))
+    thread.start()
+    return thread
+
+
+current_thread = create_thread()
+
+
+def close():
+    master.destroy()
+    raise Exception
+
+
+Label(master, text="Watch Directory").grid(row=0)
+Label(master, text="Update Every Minutes").grid(row=1)
+Label(master, text="RSS Feed").grid(row=2)
+
+watch_dir = Entry(master)
+update_after_minutes = Entry(master)
+rss_feed = Entry(master)
+
+watch_dir.grid(row=0, column=1)
+update_after_minutes.grid(row=1, column=1)
+rss_feed.grid(row=2, column=1)
+
+watch_dir.insert(END, settings.WATCH_DIR)
+update_after_minutes.insert(END, settings.UPDATE_AFTER_MINUTES)
+rss_feed.insert(END, settings.RSS_FEED)
+
+update_btn = Button(master, text='Update', command=update_settings)
+update_btn.grid(row=3, column=1)
+pause_btn = Button(master, textvariable=pause_btn_text, command=toggle_active)
+pause_btn.grid(row=4, column=1)
+
+
+log_area = Text(master)
+log_area.grid(row=5, column=1)
+log_area.config(state=DISABLED)
+
+
+def write_log(text):
+    global log_file
+    global log_area
+    text = get_time_str() + ': ' + str(text) + '\n'
+    log_area.config(state=NORMAL)
+    log_area.insert(INSERT, text)
+    log_area.config(state=DISABLED)
+
+
+master.protocol("WM_DELETE_WINDOW", close)
+master.mainloop()
--- settings.py
+++ settings.py
@@ -7,13 +7,13 @@ parser.parse()
 # where .torrent files should be copied, default value: /home/$USER/rtorrent/watch/start/
 WATCH_DIR = parser.get('WATCH_DIR')
 
-# the maximum size allowed to download. set to -1 to ignore
+# the maximum size allowed to download. set to -1 to ignore default: 50GiB
 MAX_SIZE = int(parser.get('MAX_SIZE'))
 
 # if the program should run in a loop
 AUTOMATIC_UPDATE = int(parser.get('AUTOMATIC_UPDATE')) == 1
 
-# after how many minutes the program should rerun
+# after how many minutes the program should rerun; default: 30
 UPDATE_AFTER_MINUTES = float(parser.get('UPDATE_AFTER_MINUTES'))
 
 # url of rss feed
