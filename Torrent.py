5e6ba8cb1df950a9ca2f7c2c75f0fbdfc6ce3581
ChristianGroeber <christian.groeber@bluewin.ch>
2020-12-08 12:12:21 +0100

    updated for use with blutopia

--- Torrent.py
+++ Torrent.py
@@ -1,11 +1,26 @@
 import settings
+import re
 
 
 class Torrent:
     def __init__(self, rss):
+        self.summary = self.parse_summary(rss['summary'])
         self.title = rss['title']
         self.link = rss['link']
-        self.summary = rss['summary']
-        self.size = self.summary.split('/ ')[2].split(' ')[0]
+        self.size = self.summary['size'].split(' ')[0]
         self.download = float(self.size) <= settings.MAX_SIZE or settings.MAX_SIZE == -1
-        self.id = int(rss['comments'].split('/')[-1])
+        self.id = int(rss['links'][0]['href'].split('/')[-1])
+
+    def parse_summary(self, summary):
+        cnt = 0
+        ret = {}
+        regex = r"(\s*?</?[a-zA-Z]*\s?/?>:?\s?)"
+        for line in summary.split('\n'):
+            line = line.lstrip()
+            line = re.split(regex, line)
+            if len(line) < 4:
+                continue
+            ret[line[2].lower()] = line[4]
+            cnt += 1
+        print(ret)
+        return ret
