44777dbd228683e1516c5abbb0f158444c485aa1
ChristianGroeber <christian.groeber@bluewin.ch>
2020-12-08 12:26:11 +0100

    bug fixes

--- Torrent.py
+++ Torrent.py
@@ -6,7 +6,7 @@ class Torrent:
     def __init__(self, rss):
         self.summary = self.parse_summary(rss['summary'])
         self.title = rss['title']
-        self.link = rss['title_detail']['base']
+        self.link = rss['links'][1]['href']
         self.size = self.summary['size'].split(' ')[0]
         self.download = float(self.size) <= settings.MAX_SIZE or settings.MAX_SIZE == -1
         self.id = int(rss['links'][0]['href'].split('/')[-1])
